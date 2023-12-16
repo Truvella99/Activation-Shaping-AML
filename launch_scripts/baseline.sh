7532a29998e994bb7c0f04d76323b8bef7703ec1
Truvella99 <gagliardo9975@gmail.com>
2023-12-16 16:48:37 +0000

    do step 0

--- launch_scripts/baseline.sh
+++ launch_scripts/baseline.sh
@@ -1,9 +1,9 @@
 target_domain=${1}
 
-python main.py \
+python /content/Activation-Shaping-AML/main.py \
 --experiment=baseline \
 --experiment_name=baseline/${target_domain}/ \
---dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
+--dataset_args="{'root': '/content/Activation-Shaping-AML/data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
 --batch_size=128 \
 --num_workers=5 \
 --grad_accum_steps=1
