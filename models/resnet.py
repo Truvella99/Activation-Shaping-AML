d28268b0075d6f0396ffa4ee61931446e864dc24
Truvella99 <gagliardo9975@gmail.com>
2024-01-21 12:04:24 +0000

    remove pretrained false

--- models/resnet.py
+++ models/resnet.py
@@ -25,7 +25,7 @@ class BaseResNet18(nn.Module):
 class ASHResNet18(nn.Module):
     def __init__(self):
         super(ASHResNet18, self).__init__()
-        self.resnet = resnet18(pretrained=False)
+        self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
         self.activations = []
 
